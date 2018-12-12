999e20a99eb7282aa083ce253f3e90dc9d78401f
Arbo Lindma <arbo.lindma@telia.ee>
2018-12-12 09:02:02 +0200

    Added Paging and Sort examples

--- src/main/java/com/guitar/db/repository/ModelRepository.java
+++ src/main/java/com/guitar/db/repository/ModelRepository.java
@@ -9,6 +9,10 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.db.model.Model;
@@ -59,8 +63,10 @@ public class ModelRepository {
 	/**
 	 * Custom finder
 	 */
-	public List<Model> getModelsByPriceRangeAndWoodType(BigDecimal lowest, BigDecimal highest, String wood) {
-		return modelJpaRepository.queryByPriceRangeAndWoodType(lowest, highest, "%" + wood + "%");
+	public Page<Model> getModelsByPriceRangeAndWoodType(BigDecimal lowest, BigDecimal highest, String wood) {
+		Sort sort = new Sort(Sort.Direction.ASC, "name"); 
+		Pageable page = new PageRequest(0, 2, sort);
+		return modelJpaRepository.queryByPriceRangeAndWoodType(lowest, highest, "%" + wood + "%", page);
 	}
 
 	/**
